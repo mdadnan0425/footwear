7425bbbd1d6b53f94276061a925e30010fc7e4a0
Adnan <66974008+mdadnan0425@users.noreply.github.com>
2023-04-07 23:00:55 +0530

    new changes done

--- src/layout/pages/about/about.jsx
+++ src/layout/pages/about/about.jsx
@@ -15,7 +15,21 @@ export default class About extends Component {
                         </ul>
                     </div>
                     <div className="right">
-                       
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                             Unde praesentium voluptas labore, suscipit, obcaecati
+                              totam  laborum saepe enim, facere aliquam provident
+                               dolor minima adipisci culpa. Sapiente doloribus
+                                consectetur quos ducimus id maiores eligendi corrupti
+                                 assumenda temporibus exercitationem vel expedita ullam
+                                  vero perferendis quam, laudantium pariatur explicab
+                                  o accusamus, recusandae tempora modi autem at vitae
+                                   nobis. Repellat illum, obcaecati quis rerum doloribus
+                                    adipisci distinctio, reiciendis maxime neque ipsa
+                                     delectus labore aliquam nesciunt nobis pariatur
+                                      ex odio molestias temporibus recusandae sequi dicta 
+                                      voluptas fuga amet esse! Consequuntur nobis veli
+                                      t aspernatur alias vero, cupiditate excepturi quidem
+                             expedita officia magnam libero dolore debitis suscipit nemo?</p>
                              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                              Unde praesentium voluptas labore, suscipit, obcaecati
                               totam  laborum saepe enim, facere aliquam provident
