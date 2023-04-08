339138f743c7317bc666d422b7802dfb620425f3
Adnan <66974008+mdadnan0425@users.noreply.github.com>
2023-04-08 23:00:52 +0530

    women card sec

--- src/layout/pages/homepage/women/women.jsx
+++ src/layout/pages/homepage/women/women.jsx
@@ -11,49 +11,7 @@ export default class Women extends Component {
         return (
             <div id ="open" className="header2">
                 <div className="sec2">
-          <div className="lady">
-            <div className="style">
-              <img src={heels} alt="highheels1" />
-              <h1>
-                Form your <span>Own Style</span>
-              </h1>
-            </div>
-            <p>Womens footwear</p>
-          </div>
-          <div className="girlscontainer">
-            <i id="arrow" class="fas fa-arrow-circle-left"></i>
-            <div className="box">
-              <i class="fas fa-shopping-cart"></i>
-              <img src={pink} alt="pinkgirl" />
-              <button>Buy Now</button>
-            </div>
-            <div className="box">
-              <i class="fas fa-shopping-cart"></i>
-              <img src={brown} alt="browngirl" />
-              <button>Buy Now</button>
-            </div>
-            <div className="box">
-              <i class="fas fa-shopping-cart"></i>
-              <img src={boot} alt="bootgirl" />
-              <button>Buy Now</button>
-            </div>
-            <div className="box">
-              <i class="fas fa-shopping-cart"></i>
-              <img src={sandle} alt="sandlegirl" />
-              <button>Buy Now</button>
-            </div>
-            <i id="arrow" class="fas fa-arrow-circle-right"></i>
-            <i id="arrow" class="fas fa-arrow-circle-left"></i>
-            <div className="box">
-              <i class="fas fa-shopping-cart"></i>
-              <img src={pink} alt="pinkgirl" />
-              <button>Buy Now</button>
-            </div>
-            <div className="box">
-              <i class="fas fa-shopping-cart"></i>
-              <img src={brown} alt="browngirl" />
-              <button>Buy Now</button>
-            </div>
+          
             <div className="box">
               <i class="fas fa-shopping-cart"></i>
               <img src={boot} alt="bootgirl" />
